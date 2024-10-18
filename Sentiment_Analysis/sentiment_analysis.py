aabb073cd921930c5f3355520705b4e5229a05ca
NYNARU KRISHNA PRIYA <priya.nynaru@gmail.com>
2024-10-19 00:00:43 +0530

    NLP-From-Scratch-to-Expert

---
+++ Sentiment_Analysis/sentiment_analysis.py
@@ -0,0 +1,64 @@
+import nltk
+import pandas as pd
+
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
+#download nltk corpus (first time only)
+# nltk.download('all')
+
+
+# Load the amazon review dataset
+df = pd.read_csv('https://raw.githubusercontent.com/pycaret/pycaret/master/datasets/amazon.csv')
+
+print(df)
+
+# Preprocessing Data
+
+def preprocess_text(text):
+
+    #Tokenize the text
+    tokens = word_tokenize(text.lower())
+
+    # Remove stop words
+    filterd_tokens = [token for token in tokens if token not in stopwords.words('english')]
+
+    #Lemmatize the tokens
+
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in filterd_tokens]
+    # Join the tokens back into a string
+
+    processed_text = ' '.join(lemmatized_tokens)
+
+    return processed_text
+
+#apply the function
+
+df['reviewText'] = df['reviewText'].apply(preprocess_text)
+print(df)
+
+# initialize NLTK sentiment analyzer
+analyzer = SentimentIntensityAnalyzer()
+
+# create get_sentiment function
+def get_sentiment(text):
+
+    scores = analyzer.polarity_scores(text)
+    sentiment = 1 if scores['pos'] > 0 else 0
+    return sentiment
+
+# apply get_sentiment function
+df['sentiment'] = df['reviewText'].apply(get_sentiment)
+
+print(df)
+
+from sklearn.metrics import confusion_matrix
+
+print(confusion_matrix(df['Positive'], df['sentiment']))
+
+from sklearn.metrics import classification_report
+
+print(classification_report(df['Positive'], df['sentiment']))
